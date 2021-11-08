c10943152671762e1b7a57561072fad6c51177fd
Vikhyat Yadav <vikhyat19127@iiitd.ac.in>
2021-11-08 12:18:15 +0530

    payments added in posts.js

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
-const bodyPars = require('body-parser');
-const https = require('https')
-const path = require('path');
-const fs = require('fs');
+const cors = require("cors");
+const bodyPars = require("body-parser");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
 require("dotenv/config");
-app.use(express.json())
-app.use( cors( {
+app.use(express.json());
+app.use(
+  cors({
     origin: "http://localhost:3000",
-    methods : ['GET', 'POST']
-}))
-console.log(process.env.DB_CONN)
+    methods: ["GET", "POST"],
+  })
+);
+console.log(process.env.DB_CONN);
 
-app.use(bodyPars.urlencoded({
-    extended:true
-}
-));
+app.use(
+  bodyPars.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyPars.json());
 //Import routes
-
 
 const postroute = require("./routes/posts");
 const auth = require("./routes/auth");
 
+app.use("/posts", postroute);
+app.use("/api/auth", auth);
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+const ssl = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "private.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "ssl_cert.pem")),
+  },
+  app
+);
 
-app.use('/posts',postroute);
-app.use("/api/auth", auth)
-app.get('/', (req,res)=>{
-    res.send("We are on home");
-})
-const ssl = https.createServer({
-    key: fs.readFileSync(path.join(__dirname,'cert','private.pem')),
-    cert:fs.readFileSync(path.join(__dirname,'cert','ssl_cert.pem'))
-},app)
-
-mongoose.connect(process.env.DB_CONN,(error)=>{
-    if(!error){
-        console.log("connection est");
-    }
-    else{
-        console.log("error connecting to db")
-    }
-})
-ssl.listen(5500,()=> console.log("ssl server live"));
+mongoose.connect(process.env.DB_CONN, (error) => {
+  if (!error) {
+    console.log("connection est");
+  } else {
+    console.log("error connecting to db");
+  }
+});
+ssl.listen(5500, () => console.log("ssl server live"));
 app.listen(5000);
