4b9d663bac9e1b36827a34244d2cf234ecc5705d
souravb786 <55682234+souravb786@users.noreply.github.com>
2021-11-10 00:07:44 +0530

    Add files via upload

---
+++ routes/detailChange.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const { user, userName, secrets } = require('../models/credential')
+
+router.post('/profile',async (req, res) => {
+    console.log(req)
+    const oldemail =  req.body.email    
+    const userDetails = await user.findOne({email: oldemail})
+    if(!userDetails){
+        return res.status(400).send({message: "Something went wrong"})
+    }
+    try{
+        const address = req.body.address
+        const newEmail = req.body.newemail
+        await userName.updateOne({email:oldemail}, {$set:{"email": newEmail}})
+        console.log(address)
+        await user.updateOne({email:oldemail}, {$set:{"address":address}})
+        await user.updateOne({email:oldemail}, {$set:{'mobile':req.body.mobile}})
+        await user.updateOne({email:oldemail}, {$set:{'email':newEmail}})
+
+        return res.send(await user.findOne({email:newEmail}))
+    }catch(err){
+        return res.send(err)
+    }
+})
+
+module.exports = router
